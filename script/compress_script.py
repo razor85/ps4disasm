f178c4b14cb6897612efb28e7194a4dd86df55a7
Benoit Goudreault-Emond <bgoudreaultemond@gmail.com>
2019-01-15 22:14:37 -0500

    Refactor compress script for future usage in formation data.

--- script/compress_script.py
+++ script/compress_script.py
@@ -14,22 +14,286 @@ MAX_TEXT_SIZE = MAX_TEXT_ADDRESS - Dialogue_Trees_ADDRESS
 # alternatively, if you're not 100% sure, max length in US script is 7220 bytes
 #MAX_TEXT_SIZE = 7220
 
+def decodenum(s):
+	if s[0] == '$':
+		return int(s[1:], 16)
+	else:
+		return int(s)
+
+class IgnoreDcProcessor:
+	def __init__(self, fname):
+		self.fname = fname
+		
+	def matches(self, line):
+		return line
+
+	def process(self, m, linenum):
+		global debug
+		if debug:
+			print(m)
+
+DC_W_DIRECTIVE = re.compile(r"^\s+dc\.w\s+((\$[A-Fa-f0-9]{4})|(\d+))\s*(;.*)?$")
+class DcWProcessor(IgnoreDcProcessor):
+
+	def __init__(self, fname, out):
+		super().__init__(fname)
+		self.out = out
+		
+	def matches(self, line):
+		self.m = DC_W_DIRECTIVE.match(line)
+		return self.m
+		
+	def process(self, m, linenum):
+		s = m.group(1)
+		decoded = decodenum(s)
+		out.write(bytes([(decoded&0xFF00)>>8, decoded&0xFF]))
+		
+DC_DIRECTIVE = re.compile(r"^\s+dc\.b\s+(.*)$")
+hexdigits = set("ABCDEFabcdef0123456789")
+class DcBProcessor(IgnoreDcProcessor):
+	
+	def __init__(self, fname, out, charset, symbols):
+		super().__init__(fname)
+		self.out = out
+		self.charset = charset
+		self.buf = []
+		self.state = self.scanning
+		
+	def matches(self, line):
+		self.m = DC_DIRECTIVE.match(line)
+		return self.m
+	
+	def process(self, m, linenum):
+		self.state = self.scanning
+		self.buf.clear()
+		for ch in m.group(1):
+			self.state = self.state(ch, linenum)
+		self.state(None, linenum)
+
+	def syntax_error(self, ch, linenum):
+		print("Syntax error: illegal character", ch, "file:", self.fname, "@", linenum)
+		sys.exit(-1)
+		
+	def comment(self, ch, linenum):
+		return self.comment
+
+	def hex_digit(self, ch, linenum):
+		if ch is not None and ch in hexdigits:
+			self.buf.append(ch)
+			return self.hex_digit
+		else:
+			translated = decodenum(''.join(self.buf))
+			out.write(bytes([translated]))
+			self.buf.clear()
+			return self.scanning(ch, linenum)
+		
+	def in_quote(self, ch, linenum):
+		if ch is None:
+			self.syntax_error("newline", linenum)
+		if ch == '"':
+			return self.maybe_quote_char
+		self.buf.append(ch)
+		return self.in_quote
+
+	def maybe_quote_char(self, ch, linenum):
+		if ch is not None and ch == '"':
+			self.buf.append('"')
+			return self.in_quote
+		else:
+			# translate unknown chars to space
+			self.out.write(bytes([self.charset.get(ch, self.charset.get(' ', 0)) for ch in self.buf]))
+			self.buf.clear()
+			return self.scanning(ch, linenum)
+
+	def dec_digit(self, ch, linenum):
+		if ch is not None and ch.isdigit():
+			self.buf.append(ch)
+			return self.dec_digit
+		else:
+			translated = decodenum(''.join(self.buf))
+			self.out.write(bytes([translated]))
+			self.buf.clear()
+			return self.scanning(ch, buf, linenum, out)
+			
+	def identifier(self, ch, linenum):
+		if ch is None or ch == ',' or ch.isspace():
+			sym = ''.join(self.buf)
+			val = self.symbols[sym]
+			# split in bytes
+			if val < 0xFF:
+				self.syntax_error(sym, linenum)
+				
+			self.out.write(bytes([val]))
+			return self.scanning
+		elif ch.isalnum():
+			self.buf.append(ch)
+			return self.identifier
+		else:
+			self.syntax_error(ch, linenum)
+
+	def scanning(self, ch, linenum):
+		if ch is None:
+			return
+		if ch == '$':
+			self.buf.append('$')
+			return self.hex_digit
+		if ch == ',':
+			return self.scanning
+		if ch == '"':
+			return self.in_quote
+		if ch == ';':
+			return self.comment
+		if ch.isdigit():
+			self.buf.append(ch)
+			return self.dec_digit
+		if ch.isidentifier():
+			self.buf.append(ch)
+			return self.identifier
+		if ch.isspace():
+			return self.scanning
+		self.syntax_error(ch, linenum)
+		
+class CompositeDcProcessor:
+	def __init__(self, fname, out, charset, symbols):
+		self.delegate = [DcWProcessor(fname, out),
+				 DcBProcessor(fname, out, charset, symbols),
+				 IgnoreDcProcessor(fname)]
+		self.current = None
+	
+	def matches(self, line):
+		self.current = None
+		for d in self.delegate:
+			m = d.matches(line)
+			if m:
+				self.current = d
+				return m
+	
+	def process(self, m, linenum):
+		self.current.process(m, linenum)
+		self.current = None
+
 
 CHARSET_RANGE = re.compile(r"^\s+charset\s+'(.)'\s*,\s*'(.)'\s*,\s*((\$[A-Fa-f0-9][A-Fa-f0-9])|(\d+))\s*(;.*)?$")
 CHARSET_SINGLE = re.compile(r"^\s+charset\s+'(.)'\s*,\s*((\$[A-Fa-f0-9][A-Fa-f0-9])|(\d+))\s*(;.*)?$")
 CHARSET_SINGLE_NUM = re.compile(r"^\s+charset\s+(\$[A-Fa-f0-9][A-Fa-f0-9])\s*,\s*((\$[A-Za-z0-9][A-Za-z0-9])|(\d+))\s*(;.*)?$")
+class CharsetProcessor:
+	
+	def __init__(self, fname, charset, rcharset):
+		self.fname = fname
+		self.charset = charset
+		self.rcharset = rcharset
+		self.linenum = 0
+	
+	def process(self, line):
+		self.linenum += 1
+		r = CHARSET_RANGE.match(line)
+		if r:
+			base = decodenum(r.group(3))
+			for count, ch in enumerate(range(ord(r.group(1)), ord(r.group(2)) + 1)):
+				self.charset[chr(ch)] = base + count
+				self.rcharset[base+count] = chr(ch)
+			return
+			
+		single = CHARSET_SINGLE.match(line)
+		if single:
+			self.charset[single.group(1)] = decodenum(single.group(2))
+			self.rcharset[decodenum(single.group(2))] = single.group(1)
+			return
+			
+		single_num = CHARSET_SINGLE_NUM.match(line)
+		if single_num:
+			self.charset[chr(decodenum(single_num.group(1)))] = decodenum(single_num.group(2))
+			self.rcharset[decodenum(single_num.group(2))] = chr(decodenum(single_num.group(1)))
+			return
+			
+		print("Invalid charset line; file:", self.fname, "@", self.linenum, "- ignoring")
 
+
+ASSIGN_DIRECTIVE = re.compile(r"^\s*([A-Za-z_][A-Za-z0-9_]*)\s*=\s*((\$[A-Fa-f0-9]+)|(\d+))\s*(;.*)?$")
 IF_DIRECTIVE = re.compile(r"^\s+if\s+([A-Za-z0-9_]+)\s*=\s*([0-9]+)\s*$")
 ELSE_DIRECTIVE = re.compile(r"^\s+else\s*$")
 ENDIF_DIRECTIVE = re.compile(r"^\s+endif\s*$")
-DC_DIRECTIVE = re.compile(r"^\s+dc\.b\s+(.*)$")
-DC_W_DIRECTIVE = re.compile(r"^\s+dc\.w\s+((\$[A-Fa-f0-9]{4})|(\d+))\s*(;.*)?$")
+class Processor:
+	def __init__(self, filename, symbols, dc_processor):
+		self.filename = filename
+		self.linenum = 1
+		self.state = self.normal
+		self.symbols = symbols
+		self.dc_processor = dc_processor
+		
+	def process(self, line):
+		if self.state is None:
+			return
+		self.state = self.state(line)
+		self.linenum += 1
+		
+	def check(self):
+		if self.state != self.normal:
+			abort("unterminated if", self.filename)
+	
+	def normal(self, line):
+		m = IF_DIRECTIVE.match(line)
+		if m:
+			var, val = m.group(1), m.group(2)
+			if val == self.symbols.get(var, None):
+				return look_for_else
+			else:
+				return look_for_else_skip
+		self.process_non_controlflow(line)
+		return self.normal
+		
+	def look_for_else_skip(self, line):
+		m = ELSE_DIRECTIVE.match(line)
+		if m:
+			return self.look_for_endif
+		m = ENDIF_DIRECTIVE.match(line)
+		if m:
+			return self.normal
+		return self.look_for_else_skip
+		
+	def look_for_else(self, line):
+		m = ELSE_DIRECTIVE.match(line)
+		if m:
+			return self.look_for_endif_skip
+		m = ENDIF_DIRECTIVE.match(line)
+		if m:
+			return self.normal
+		self.process_non_controlflow(line)
+		return self.look_for_else
+			
+	def look_for_endif_skip(self, line):
+		m = ENDIF_DIRECTIVE.match(line)
+		if m:
+			return self.normal
+		return self.look_for_endif_skip
 
-charset = {}
-rcharset = {0xff: '\n-----\n', 0xfc: '\n', 0xfd: '\nVV\n'}
+	def look_for_endif(self, line):
+		m = ENDIF_DIRECTIVE.match(line)
+		if m:
+			return self.normal
+		self.process_non_controlflow(line)
+		return look_for_endif
+			
+	def process_non_controlflow(self, line):
+		m = ASSIGN_DIRECTIVE.match(line)
+		if m:
+			self.symbols[m.group(1)] = decodenum(m.group(2))
+			return
+		
+		m = self.dc_processor.matches(line)
+		if m:
+			self.dc_processor.process(m, self.linenum)
+			
+	def abort(self, s, filename):
+		print(s, "file:", self.fname, "@", self.linenum)
+		sys.exit(-1)
 
-symbols = [a for a in sys.argv[1:] if '=' in a]
-symbols = {arg: val for arg, val in map(lambda s: s.split('='), symbols)}
+basedir = pathlib.Path(sys.argv[0]).resolve().parent
+print("Working directory:", basedir)
+
+
+cmd_symbols = [a for a in sys.argv[1:] if '=' in a]
+cmd_symbols = {arg: val for arg, val in map(lambda s: s.split('='), cmd_symbols)}
 remainder = set([a for a in sys.argv[1:] if '=' not in a])
 debug = False
 no_compress = False
@@ -43,39 +307,32 @@ if "--no-compress" in remainder:
 	
 remainder = [pathlib.Path(f) for f in remainder]
 
+symbols = {}
+
+# load symbols from ps4.options.asm in directory one above
+fname = basedir.parent / "ps4.options.asm"
+if fname.exists():
+	with open(fname, "rt", encoding="iso-8859-1") as f:
+		processor = Processor(fname, symbols, IgnoreDcProcessor(fname))
+		for line in f:
+			line = line.rstrip()
+			processor.process(line)
+
+# override with command line settings
+symbols.update(cmd_symbols)
+
 print("Processing using symbols:", symbols)
 
-basedir = pathlib.Path(sys.argv[0]).resolve().parent
-print("Working directory:", basedir)
+charset = {}
+rcharset = {0xff: '\n-----\n', 0xfc: '\n', 0xfd: '\nVV\n'}
 
-def decodenum(s):
-	if s[0] == '$':
-		return int(s[1:], 16)
-	else:
-		return int(s)
-
-with open(basedir / "charset.asm", "rt", encoding="iso-8859-1") as f:
+fname = basedir / "charset.asm"
+with open(fname, "rt", encoding="iso-8859-1") as f:
+	charset_processor = CharsetProcessor(fname, charset, rcharset)
 	for line in f:
 		line = line.rstrip()
-		r = CHARSET_RANGE.match(line)
-		if r:
-			base = decodenum(r.group(3))
-			for count, ch in enumerate(range(ord(r.group(1)), ord(r.group(2)) + 1)):
-				charset[chr(ch)] = base + count
-				rcharset[base+count] = chr(ch)
-			continue
-			
-		single = CHARSET_SINGLE.match(line)
-		if single:
-			charset[single.group(1)] = decodenum(single.group(2))
-			rcharset[decodenum(single.group(2))] = single.group(1)
-			continue
-			
-		single_num = CHARSET_SINGLE_NUM.match(line)
-		if single_num:
-			charset[chr(decodenum(single_num.group(1)))] = decodenum(single_num.group(2))
-			rcharset[decodenum(single_num.group(2))] = chr(decodenum(single_num.group(1)))
-
+		charset_processor.process(line)
+		
 print("Using charset table:")
 for char, value in charset.items():
 	if char.isprintable():
@@ -83,134 +340,6 @@ for char, value in charset.items():
 	else:
 		print('\t${0:02X}: {1:02X}'.format(ord(char[0]), value))
 
-def normal(line, linenum, out):
-	m = IF_DIRECTIVE.match(line)
-	if m:
-		var, val = m.group(1), m.group(2)
-		if val == symbols.get(var, None):
-			return look_for_else
-		else:
-			return look_for_else_skip
-	process_raw_dc(line, linenum, out)
-	return normal
-	
-def look_for_else_skip(line, linenum, out):
-	m = ELSE_DIRECTIVE.match(line)
-	if m:
-		return look_for_endif
-	m = ENDIF_DIRECTIVE.match(line)
-	if m:
-		return normal
-	return look_for_else_skip
-	
-def look_for_else(line, linenum, out):
-	m = ELSE_DIRECTIVE.match(line)
-	if m:
-		return look_for_endif_skip
-	m = ENDIF_DIRECTIVE.match(line)
-	if m:
-		return normal
-	process_raw_dc(line, linenum, out)
-	return look_for_else
-		
-def look_for_endif_skip(line, linenum, out):
-	m = ENDIF_DIRECTIVE.match(line)
-	if m:
-		return normal
-	return look_for_endif_skip
-
-def look_for_endif(line, linenum, out):
-	m = ENDIF_DIRECTIVE.match(line)
-	if m:
-		return normal
-	process_raw_dc(line, linenum, out)
-	return look_for_endif
-		
-def process_raw_dc(line, linenum, out):
-	m = DC_DIRECTIVE.match(line)
-	if m:
-		process_dc(m.group(1), linenum, out)
-		return
-	m = DC_W_DIRECTIVE.match(line)
-	if m:
-		s = m.group(1)
-		decoded = decodenum(s)
-		out.write(bytes([(decoded&0xFF00)>>8,decoded&0xFF]))
-	else:
-		#print(line)
-		pass
-
-def process_dc(dc, linenum, out):
-	state = scanning
-	buf = []
-	for ch in dc:
-		state = state(ch, buf, linenum, out)
-	state(None, buf, linenum, out)
-
-def syntax_error(ch, linenum):
-	print("Syntax error: illegal character", ch, "@", linenum)
-	sys.exit(-1)
-	
-def comment(ch, buf, linenum, out):
-	return comment
-
-hexdigits = set("ABCDEFabcdef0123456789")
-def hex_digit(ch, buf, linenum, out):
-	if ch is not None and ch in hexdigits:
-		buf.append(ch)
-		return hex_digit
-	else:
-		translated = decodenum(''.join(buf))
-		out.write(bytes([translated]))
-		buf.clear()
-		return scanning(ch, buf, linenum, out)
-	
-def in_quote(ch, buf, linenum, out):
-	if ch is None:
-		syntax_error("newline", linenum)
-	if ch == '"':
-		return maybe_quote_char
-	buf.append(ch)
-	return in_quote
-
-def maybe_quote_char(ch, buf, linenum, out):
-	if ch is not None and ch == '"':
-		buf.append('"')
-		return in_quote
-	else:
-		# translate unknown chars to space
-		out.write(bytes([charset.get(ch, charset.get(' ', 0)) for ch in buf]))
-		buf.clear()
-		return scanning(ch, buf, linenum, out)
-
-def dec_digit(ch, buf, linenum, out):
-	if ch is not None and ch.isdigit():
-		buf.append(ch)
-		return dec_digit
-	else:
-		translated = decodenum(''.join(buf))
-		out.write(bytes([translated]))
-		buf.clear()
-		return scanning(ch, buf, linenum, out)
-
-def scanning(ch, buf, linenum, out):
-	if ch is None:
-		return
-	if ch == '$':
-		buf.append('$')
-		return hex_digit
-	if ch == ',':
-		return scanning
-	if ch == '"':
-		return in_quote
-	if ch == ';':
-		return comment
-	if ch.isdigit():
-		buf.append(ch)
-		return dec_digit
-	if ch.isspace():
-		return scanning
-	syntax_error(ch, linenum)
 	
 def dump(f):
 	print(f)
@@ -225,6 +354,7 @@ def dump(f):
 
 if len(remainder) == 0:
 	remainder = basedir.glob('dialogue *.asm')
+
 for fname in remainder:
 	print("processing", fname)
 	dest = fname.with_suffix('.bin.unc')
@@ -236,16 +366,15 @@ for fname in remainder:
 	if idx != 0:
 		print("backing up", dest, "to", dest2)
 		dest.rename(dest2)
-	state = normal
 	with open(fname, "rt", encoding="iso-8859-1") as f, open(dest, "wb") as out:
-		for linenum, line in enumerate(f):
+		processor = Processor(fname, symbols, CompositeDcProcessor(fname, out, charset, symbols))
+		for line in f:
 			line = line.rstrip()
-			state = state(line, linenum, out)
+			processor.process(line)
 			if debug:
 				print(out.tell(), ':', line)
-		if state != normal:
-			print("Unterminated if in ", fname, ", aborting", sep='')
-			sys.exit(-1)
+		processor.check()
+		# todo: make that part of the processor somehow
 		if out.tell() % 2 != 0:
 			out.write(bytes([0]))
 		print("done processing", fname, "to", dest)
@@ -254,6 +383,7 @@ for fname in remainder:
 			print("You may need to relocate the decompression buffer.", file=sys.stderr, sep='')
 			print("Press enter to continue.", file=sys.stderr, sep='')
 			input()
+
 	if debug:
 		dump(dest)
 		if dest2.exists():
