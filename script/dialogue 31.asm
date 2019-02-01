90a431e4557fd37f9a0669e4ba561475a9b07a0c
Lorenzo Mangione <lory2004@gmail.com>
2019-02-01 10:50:57 +0100

    Dialogue 31 format fix

--- script/dialogue 31.asm
+++ script/dialogue 31.asm
@@ -1,19 +1,19 @@
 ; 0
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" I wonder where Alys is?"
+	dc.b	$01, $00
+	dc.b	"I wonder where Alys is?"
 	dc.b	$FF
 
 ; $1
 	dc.b	$F4
-	dc.b	$02
-	dc.b	" We better see the principal"
+	dc.b	$02, $00
+	dc.b	"We better see the principal"
 	dc.b	$FC
 	dc.b	"and get more details!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	"ASo he's the one who requested"
+	dc.b	$01, $01
+	dc.b	"So he's the one who requested"
 	dc.b	$FC
 	dc.b	"the commission."
 	dc.b	$FD
@@ -22,16 +22,16 @@
 
 ; $2
 	dc.b	$F4
-	dc.b	$01
-	dc.b	"AAre you saying that there are"
+	dc.b	$01, $01
+	dc.b	"Are you saying that there are"
 	dc.b	$FC
 	dc.b	"monsters in the basement of this"
 	dc.b	$FD
 	dc.b	"academy?"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$02
-	dc.b	" Hmm..."
+	dc.b	$02, $00
+	dc.b	"Hmm..."
 	dc.b	$FD
 	dc.b	"Sounds like there's something"
 	dc.b	$FC
@@ -40,42 +40,42 @@
 
 ; $3
 	dc.b	$F4
-	dc.b	$02
-	dc.b	" Both the principal and Hahn"
+	dc.b	$02, $00
+	dc.b	"Both the principal and Hahn"
 	dc.b	$FC
 	dc.b	"have asked us to inspect"
 	dc.b	$FD
 	dc.b	"the basement."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$03
-	dc.b	"BAre you sure you're not just"
+	dc.b	$03, $02
+	dc.b	"Are you sure you're not just"
 	dc.b	$FC
 	dc.b	"trying to take my money?"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	"AHey, you've already paid, so"
+	dc.b	$01, $01
+	dc.b	"Hey, you've already paid, so"
 	dc.b	$FC
 	dc.b	"don't cry over spilt milk."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$03
-	dc.b	"BI wonder if I'll be OK"
+	dc.b	$03, $02
+	dc.b	"I wonder if I'll be OK"
 	dc.b	$FC
 	dc.b	"going with these people..."
 	dc.b	$FF
 
 ; $4
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" I wonder what that capsules"
+	dc.b	$01, $00
+	dc.b	"I wonder what that capsules"
 	dc.b	$FC
 	dc.b	"in the basement are for?"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$02
-	dc.b	"AI'm going to make the principal"
+	dc.b	$02, $01
+	dc.b	"I'm going to make the principal"
 	dc.b	$FC
 	dc.b	"tell us!"
 	dc.b	$FD
@@ -84,14 +84,14 @@
 	dc.b	"You're coming along too!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$03
-	dc.b	"BWho m...me?"
+	dc.b	$03, $02
+	dc.b	"Who m...me?"
 	dc.b	$FF
 
 ; $5
 	dc.b	$F4
-	dc.b	$03
-	dc.b	"ABirth Valley is located"
+	dc.b	$03, $01
+	dc.b	"Birth Valley is located"
 	dc.b	$FC
 	dc.b	"northeast of Piata."
 	dc.b	$FD
@@ -100,110 +100,110 @@
 	dc.b	"professor's rescue!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$02
-	dc.b	" Ah...Zio."
+	dc.b	$02, $00
+	dc.b	"Ah...Zio."
 	dc.b	$FC
 	dc.b	"I don't like the sound of that!"
 	dc.b	$FF
 
 ; $6
 	dc.b	$F4
-	dc.b	$03
-	dc.b	"BOh, no! Professor Holt has been"
+	dc.b	$03, $02
+	dc.b	"Oh, no! Professor Holt has been"
 	dc.b	$FC
 	dc.b	"turned to stone!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" He'll be all right, Hahn."
+	dc.b	$01, $00
+	dc.b	"He'll be all right, Hahn."
 	dc.b	$FD
 	dc.b	"We can go to Molcum and obtain"
 	dc.b	$FC
 	dc.b	"the medecine Alshline."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$03
-	dc.b	"BMolcum...you mean that Motavian"
+	dc.b	$03, $02
+	dc.b	"Molcum...you mean that Motavian"
 	dc.b	$FC
 	dc.b	"village south of Zema?"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$02
-	dc.b	"AOh, calm down!"
+	dc.b	$02, $01
+	dc.b	"Oh, calm down!"
 	dc.b	$FC
 	dc.b	"Really, you're pathetic!"
 	dc.b	$FF
 
 ; $7
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" Molcum has been"
+	dc.b	$01, $00
+	dc.b	"Molcum has been"
 	dc.b	$FC
 	dc.b	"done in by Zio..."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$04
-	dc.b	"ABy the time I arrived,"
+	dc.b	$04, $01
+	dc.b	"By the time I arrived,"
 	dc.b	$FC
 	dc.b	"it was too late."
 	dc.b	$FD
 	dc.b	"Sorry."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$02
-	dc.b	" Hmmm..."
+	dc.b	$02, $00
+	dc.b	"Hmmm..."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$03
-	dc.b	"BBut, once we get to Tonoe,"
+	dc.b	$03, $02
+	dc.b	"But, once we get to Tonoe,"
 	dc.b	$FC
 	dc.b	"we'll be able to obtain"
 	dc.b	$FD
 	dc.b	"Alshline, right?"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" Tonoe...now that's north"
+	dc.b	$01, $00
+	dc.b	"Tonoe...now that's north"
 	dc.b	$FC
 	dc.b	"of the village of Krup, right?"
 	dc.b	$FF
 
 ; $8
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" So there's Alshline in the"
+	dc.b	$01, $00
+	dc.b	"So there's Alshline in the"
 	dc.b	$FC
 	dc.b	"underground warehouse of Tonoe?"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$02
-	dc.b	"AThanks for"
+	dc.b	$02, $01
+	dc.b	"Thanks for"
 	dc.b	$FC
 	dc.b	"being our guide, Gryz."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$05
-	dc.b	"BNo problem."
+	dc.b	$05, $02
+	dc.b	"No problem."
 	dc.b	$FC
 	dc.b	"You can leave everything to me."
 	dc.b	$FF
 
 ; $9
 	dc.b	$F4
-	dc.b	$02
-	dc.b	" We've finally obtained"
+	dc.b	$02, $00
+	dc.b	"We've finally obtained"
 	dc.b	$FC
 	dc.b	"the Alshline!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$03
-	dc.b	"ALet's hurry to Zema and rescue"
+	dc.b	$03, $01
+	dc.b	"Let's hurry to Zema and rescue"
 	dc.b	$FC
 	dc.b	"the professor and his team."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$05
-	dc.b	"BI'll join you."
+	dc.b	$05, $02
+	dc.b	"I'll join you."
 	dc.b	$FD
 	dc.b	$F4
 	dc.b	$01
@@ -212,15 +212,15 @@
 
 ; $A
 	dc.b	$F4
-	dc.b	$01
-	dc.b	"AProfessor Holt has gone into"
+	dc.b	$01, $01
+	dc.b	"Professor Holt has gone into"
 	dc.b	$FC
 	dc.b	"the depths of Birth Valley and"
 	dc.b	$FD
 	dc.b	"hasn't come back since."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$03
+	dc.b	$03, $02
 	dc.b	"BAfter all we did to obtain"
 	dc.b	$FC
 	dc.b	"the Alshline to turn him from"
@@ -228,36 +228,36 @@
 	dc.b	"stone back to flesh."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$02
-	dc.b	" This teacher of yours is such a"
+	dc.b	$02, $00
+	dc.b	"This teacher of yours is such a"
 	dc.b	$FC
 	dc.b	"pain in the butt!"
 	dc.b	$FF
 
 ; $B
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"AWe've got to hurry and stop"
+	dc.b	$06, $01
+	dc.b	"We've got to hurry and stop"
 	dc.b	$FC
 	dc.b	"the system that's running out of"
 	dc.b	$FD
 	dc.b	"control...for Seed's sake, too!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" First, we'll have to rescue"
+	dc.b	$01, $00
+	dc.b	"First, we'll have to rescue"
 	dc.b	$FC
 	dc.b	"this android called Demi."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$05
-	dc.b	"BWe'll have to go to Zio's Fort."
+	dc.b	$05, $02
+	dc.b	"We'll have to go to Zio's Fort."
 	dc.b	$FC
 	dc.b	"Just what we all wanted to do!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$03
-	dc.b	"AYou're talking about that castle"
+	dc.b	$03, $01
+	dc.b	"You're talking about that castle"
 	dc.b	$FC
 	dc.b	"beyond the quicksand that can be"
 	dc.b	$FD
@@ -270,22 +270,22 @@
 
 ; $C
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" Oh no!"
+	dc.b	$01, $00
+	dc.b	"Oh no!"
 	dc.b	$FC
 	dc.b	"Alys is..."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"AChaz, let's do as Alys says"
+	dc.b	$06, $01
+	dc.b	"Chaz, let's do as Alys says"
 	dc.b	$FC
 	dc.b	"and find this person called"
 	dc.b	$FD
 	dc.b	"Rune."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$07
-	dc.b	"BWe'll need the 'Land Rover'"
+	dc.b	$07, $02
+	dc.b	"We'll need the 'Land Rover'"
 	dc.b	$FC
 	dc.b	"to cross the quicksand."
 	dc.b	$FD
@@ -298,8 +298,8 @@
 
 ; $D
 	dc.b	$F4
-	dc.b	$07
-	dc.b	" The Land Rover is"
+	dc.b	$07, $00
+	dc.b	"The Land Rover is"
 	dc.b	$FC
 	dc.b	"inside the Machine Center!"
 	dc.b	$FD
@@ -308,28 +308,28 @@
 
 ; $E
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" OK! "
+	dc.b	$01, $00
+	dc.b	"OK! "
 	dc.b	$FC
 	dc.b	"We've got the Land Rover!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$05
-	dc.b	"BNow we can cross the quicksand"
+	dc.b	$05, $02
+	dc.b	"Now we can cross the quicksand"
 	dc.b	$FC
 	dc.b	"to the east and get to"
 	dc.b	$FD
 	dc.b	"The Ladea Tower."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"AAnd there's supposed to be"
+	dc.b	$06, $01
+	dc.b	"And there's supposed to be"
 	dc.b	$FC
 	dc.b	"a person called Rune there."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" I don't feel much like seeing"
+	dc.b	$01, $00
+	dc.b	"I don't feel much like seeing"
 	dc.b	$FC
 	dc.b	"him,"
 	dc.b	$FD
@@ -338,22 +338,22 @@
 
 ; $F
 	dc.b	$F4
-	dc.b	$04
-	dc.b	" To break through Zio's magic"
+	dc.b	$04, $00
+	dc.b	"To break through Zio's magic"
 	dc.b	$FC
 	dc.b	"barrier,"
 	dc.b	$FD
 	dc.b	"we'll need the 'Psycho-wand.'"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"BAnd it's on the topmost floor"
+	dc.b	$06, $02
+	dc.b	"And it's on the topmost floor"
 	dc.b	$FC
 	dc.b	"of The Ladea Tower?"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	"ALet's defeat Zio"
+	dc.b	$01, $01
+	dc.b	"Let's defeat Zio"
 	dc.b	$FC
 	dc.b	"with that wand!"
 	dc.b	$FD
@@ -364,28 +364,28 @@
 
 ; $10
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" We've defeated Zio's underlings,"
+	dc.b	$01, $00
+	dc.b	"We've defeated Zio's underlings,"
 	dc.b	$FC
 	dc.b	"but..."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$04
-	dc.b	"AIt seems that the Psycho-wand"
+	dc.b	$04, $01
+	dc.b	"It seems that the Psycho-wand"
 	dc.b	$FC
 	dc.b	"works well against him."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$05
-	dc.b	"BLet's get it quickly before"
+	dc.b	$05, $02
+	dc.b	"Let's get it quickly before"
 	dc.b	$FC
 	dc.b	"the next ones come."
 	dc.b	$FF
 
 ; $11
 	dc.b	$F4
-	dc.b	$04
-	dc.b	"AAs long as we have"
+	dc.b	$04, $01
+	dc.b	"As long as we have"
 	dc.b	$FC
 	dc.b	"the Psycho-wand,"
 	dc.b	$FD
@@ -394,24 +394,24 @@
 	dc.b	"Zio's power."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$07
-	dc.b	"BLet's go to Nurvus in"
+	dc.b	$07, $02
+	dc.b	"Let's go to Nurvus in"
 	dc.b	$FC
 	dc.b	"the basement of Zio's Fort...!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" Blast! Alys...!"
+	dc.b	$01, $00
+	dc.b	"Blast! Alys...!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"AChaz..."
+	dc.b	$06, $01
+	dc.b	"Chaz..."
 	dc.b	$FF
 
 ; $12
 	dc.b	$F4
-	dc.b	$04
-	dc.b	"AWe've broken the invisible"
+	dc.b	$04, $01
+	dc.b	"We've broken the invisible"
 	dc.b	$FC
 	dc.b	"barrier."
 	dc.b	$FD
@@ -420,8 +420,8 @@
 	dc.b	"Nurvus!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$07
-	dc.b	"BWe can't abandon the situation"
+	dc.b	$07, $02
+	dc.b	"We can't abandon the situation"
 	dc.b	$FC
 	dc.b	"with Zio, but yes,"
 	dc.b	$FD
@@ -430,24 +430,24 @@
 	dc.b	"priority!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" Yes, that's true..."
+	dc.b	$01, $00
+	dc.b	"Yes, that's true..."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"AChaz..."
+	dc.b	$06, $01
+	dc.b	"Chaz..."
 	dc.b	$FF
 
 ; $13
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" We've been able to stop Nurvus,"
+	dc.b	$01, $00
+	dc.b	"We've been able to stop Nurvus,"
 	dc.b	$FC
 	dc.b	"thanks to Demi, but..."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"AWe've got to go to the"
+	dc.b	$06, $01
+	dc.b	"We've got to go to the"
 	dc.b	$FC
 	dc.b	"artificial satellite 'Zelan' to"
 	dc.b	$FD
@@ -458,24 +458,24 @@
 	dc.b	"And Wren is there, right?"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$04
-	dc.b	"AWhatever."
+	dc.b	$04, $01
+	dc.b	"Whatever."
 	dc.b	$FC
 	dc.b	"Let's just hurry to 'Zelan!'"
 	dc.b	$FF
 
 ; $14
 	dc.b	$F4
-	dc.b	$08
-	dc.b	" The cause of the system's"
+	dc.b	$08, $00
+	dc.b	"The cause of the system's"
 	dc.b	$FC
 	dc.b	"abnormalities turns out to be"
 	dc.b	$FD
 	dc.b	"in 'Kuran.'"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"BThe artificial satellite 'Kuran'"
+	dc.b	$06, $02
+	dc.b	"The artificial satellite 'Kuran'"
 	dc.b	$FC
 	dc.b	"...what did you say was"
 	dc.b	$FD
@@ -484,24 +484,24 @@
 
 ; $15
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" To get to Kuran,"
+	dc.b	$01, $00
+	dc.b	"To get to Kuran,"
 	dc.b	$FD
 	dc.b	"we'll have to find that"
 	dc.b	$FC
 	dc.b	"space ship Raja told us about."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"BSo that means we're going to"
+	dc.b	$06, $02
+	dc.b	"So that means we're going to"
 	dc.b	$FC
 	dc.b	"the town of Tyler northwest of"
 	dc.b	$FD
 	dc.b	"Raja's temple?"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$09
-	dc.b	"ABefore doing that,"
+	dc.b	$09, $01
+	dc.b	"Before doing that,"
 	dc.b	$FC
 	dc.b	"why don't we take a little break"
 	dc.b	$FD
@@ -514,116 +514,116 @@
 	dc.b	"you won't regret it, ha, ha!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" Great..."
+	dc.b	$01, $00
+	dc.b	"Great..."
 	dc.b	$FF
 
 ; $16
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" According to Gyuna's story,"
+	dc.b	$01, $00
+	dc.b	"According to Gyuna's story,"
 	dc.b	$FD
 	dc.b	"the space ship is underground"
 	dc.b	$FC
 	dc.b	"in the town of Tyler."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"BI hear there's some secret"
+	dc.b	$06, $02
+	dc.b	"I hear there's some secret"
 	dc.b	$FC
 	dc.b	"with the grave..."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$09
-	dc.b	"AThis seems like a planned"
+	dc.b	$09, $01
+	dc.b	"This seems like a planned"
 	dc.b	$FC
 	dc.b	"turn of events. Ha, ha!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" Hmm..."
+	dc.b	$01, $00
+	dc.b	"Hmm..."
 	dc.b	$FF
 
 ; $17
 	dc.b	$F4
-	dc.b	$04
-	dc.b	"AWell, we've found the 'Landale'"
+	dc.b	$04, $01
+	dc.b	"Well, we've found the 'Landale'"
 	dc.b	$FD
 	dc.b	"so we'll just have to go to"
 	dc.b	$FC
 	dc.b	"Kuran."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"BFinally!"
+	dc.b	$06, $02
+	dc.b	"Finally!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$08
-	dc.b	"ALet's immediately identify"
+	dc.b	$08, $01
+	dc.b	"Let's immediately identify"
 	dc.b	$FC
 	dc.b	"reason the environmental control"
 	dc.b	$FD
 	dc.b	"system is out of control."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" OK. Let's go!"
+	dc.b	$01, $00
+	dc.b	"OK. Let's go!"
 	dc.b	$FF
 
 ; $18
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"BWe've beaten Dark Force,"
+	dc.b	$06, $02
+	dc.b	"We've beaten Dark Force,"
 	dc.b	$FC
 	dc.b	"but Dezolis is not recovering!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$09
-	dc.b	"AI've been telling you,"
+	dc.b	$09, $01
+	dc.b	"I've been telling you,"
 	dc.b	$FD
 	dc.b	"the problem lies in"
 	dc.b	$FC
 	dc.b	"the Garuberk Tower!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$08
-	dc.b	"AIn any event, I do acknowledge"
+	dc.b	$08, $01
+	dc.b	"In any event, I do acknowledge"
 	dc.b	$FC
 	dc.b	"there's a need to investigate"
 	dc.b	$FD
 	dc.b	"Dezolis."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$06
-	dc.b	"BWell, what are we waiting for?"
+	dc.b	$06, $02
+	dc.b	"Well, what are we waiting for?"
 	dc.b	$FC
 	dc.b	"Let's go!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	" Hmm...the Garuberk Tower..."
+	dc.b	$01, $00
+	dc.b	"Hmm...the Garuberk Tower..."
 	dc.b	$FF
 
 ; $19
 	dc.b	$F4
-	dc.b	$06
-	dc.b	" Apparently Kyra from Esper has"
+	dc.b	$06, $00
+	dc.b	"Apparently Kyra from Esper has"
 	dc.b	$FC
 	dc.b	"headed towards the Garuberk"
 	dc.b	$FD
 	dc.b	"Tower alone!"
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$04
-	dc.b	"BI've heard the forest"
+	dc.b	$04, $02
+	dc.b	"I've heard the forest"
 	dc.b	$FC
 	dc.b	"surrounding the tower is"
 	dc.b	$FD
 	dc.b	"full of carnivorous trees."
 	dc.b	$FD
 	dc.b	$F4
-	dc.b	$01
-	dc.b	"AWe can't let her go alone."
+	dc.b	$01, $01
+	dc.b	"We can't let her go alone."
 	dc.b	$FC
 	dc.b	"We've got to go help her!"
 	dc.b	$FF
