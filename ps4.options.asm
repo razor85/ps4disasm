41e6812336a97c65c52c78eb31b7630e037095ba
Lorenzo Mangione <lory2004@gmail.com>
2019-09-22 21:42:34 +0200

    Dialogue uncompressed option to 1

--- ps4.options.asm
+++ ps4.options.asm
@@ -2,4 +2,4 @@
 bugfixes = 1			; if 1, include bug fixes
 optional_fixes  = 1		; if 1, include optional (larger) fixes
 restore_unused_enemies = 1	; if 1, Acacia and Shadow Mirage will show up
-dialogue_uncompressed = 0	; if 1, dialogue is stored uncompressed
+dialogue_uncompressed = 1	; if 1, dialogue is stored uncompressed
