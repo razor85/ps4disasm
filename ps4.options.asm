242eb4d163fcab4f4fc83f7fc0dfe70a6a7f0d79
Lorenzo Mangione <lory2004@gmail.com>
2020-07-18 11:05:04 +0200

    Removed WIP note for Revision option

--- ps4.options.asm
+++ ps4.options.asm
@@ -2,4 +2,4 @@
 bugfixes = 0			; if 1, include bug fixes
 optional_fixes  = 0		; if 1, include optional (larger) fixes
 dialogue_uncompressed = 0	; if 1, dialogue is stored uncompressed
-revision = 1			; 0 = Japanese (WIP); 1 = English; 2 = European
+revision = 1			; 0 = Japanese; 1 = English; 2 = European
