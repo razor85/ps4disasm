1a7840a119d4f978ad3a6a8a861a7bef1959d76d
Lorenzo Mangione <lory2004@gmail.com>
2020-07-31 15:15:15 +0200

    Implemented patch for light sensitivity

--- ps4.options.asm
+++ ps4.options.asm
@@ -3,3 +3,4 @@ bugfixes = 0			; if 1, include bug fixes
 optional_fixes  = 0		; if 1, include optional (larger) fixes
 dialogue_uncompressed = 0	; if 1, dialogue is stored uncompressed
 revision = 1			; 0 = Japanese; 1 = English; 2 = European
+light_sensitivity_patch = 0	; if 1, tone down flashing effects; values are taken from the Virtual Console version
